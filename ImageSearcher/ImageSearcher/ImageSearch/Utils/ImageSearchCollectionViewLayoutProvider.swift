2f1e5ed63d6a9376b7049038ce51c536e7a7d0cc
SEONGJUN <naroti@naver.com>
2024-01-07 17:05:24 +0900

    refine createLayout

--- ImageSearcher/ImageSearcher/ImageSearch/Utils/ImageSearchCollectionViewLayoutProvider.swift
+++ ImageSearcher/ImageSearcher/ImageSearch/Utils/ImageSearchCollectionViewLayoutProvider.swift
@@ -11,40 +11,42 @@ final class ImageSearchCollectionViewLayoutProvider: CollectionViewLayoutProvida
     typealias Section = ImageSearchResultSection
     typealias Item = ImageSearchResultItem
     
+    enum Const {
+        static let itemInset = NSDirectionalEdgeInsets.init(top: 2, leading: 2, bottom: 2, trailing: 2)
+    }
+    
     func createLayout(dataSource: UICollectionViewDiffableDataSource<Section, Item>?) -> UICollectionViewLayout {
         let topLeftItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                   heightDimension: .fractionalHeight(0.55))
+                                                     heightDimension: .fractionalHeight(0.55))
         let topLeftItem = NSCollectionLayoutItem(layoutSize: topLeftItemSize)
-        topLeftItem.contentInsets = NSDirectionalEdgeInsets.init(top: 2, leading: 2, bottom: 2, trailing: 2)
+        topLeftItem.contentInsets = Const.itemInset
         
         let topLeftBottomItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                     heightDimension: .fractionalHeight(0.45))
+                                                           heightDimension: .fractionalHeight(0.45))
         let topLeftBottomItem = NSCollectionLayoutItem(layoutSize: topLeftBottomItemSize)
-        topLeftBottomItem.contentInsets = NSDirectionalEdgeInsets.init(top: 2, leading: 2, bottom: 2, trailing: 2)
+        topLeftBottomItem.contentInsets = Const.itemInset
         topLeftBottomItem.edgeSpacing = .init(leading: .none, top: .none, trailing: .none, bottom: .fixed(-24))
         
         let topRightItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                    heightDimension: .fractionalHeight(0.65))
+                                                      heightDimension: .fractionalHeight(0.65))
         let topRightItem = NSCollectionLayoutItem(layoutSize: topRightItemSize)
-        topRightItem.contentInsets = NSDirectionalEdgeInsets.init(top: 2, leading: 2, bottom: 2, trailing: 2)
+        topRightItem.contentInsets = Const.itemInset
         
         let topRightBottomItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                                                                            heightDimension: .fractionalHeight(0.35)))
-        topRightBottomItem.contentInsets = NSDirectionalEdgeInsets.init(top: 2, leading: 2, bottom: 2, trailing: 2)
+        topRightBottomItem.contentInsets = Const.itemInset
         topRightBottomItem.edgeSpacing = .init(leading: .none, top: .none, trailing: .none, bottom: .fixed(-24))
         
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                                heightDimension: .fractionalHeight(1.0))
         
-        let leftGroup = NSCollectionLayoutGroup.vertical(layoutSize:
-            NSCollectionLayoutSize.init(widthDimension: .fractionalWidth(0.5),
-                                        heightDimension: .fractionalHeight(1.0)),
-                                                         subitems: [topLeftItem, topLeftBottomItem])
+        let leftGroup = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(0.5),
+                                                                           heightDimension: .fractionalHeight(1.0)),
+                                                                           subitems: [topLeftItem, topLeftBottomItem])
         
-        let rightGroup = NSCollectionLayoutGroup.vertical(layoutSize:
-            NSCollectionLayoutSize.init(widthDimension: .fractionalWidth(0.5),
-                                        heightDimension: .fractionalHeight(1.0)),
-                                                        subitems: [topRightItem, topRightBottomItem])
+        let rightGroup = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(0.5),
+                                                                            heightDimension: .fractionalHeight(1.0)),
+                                                                            subitems: [topRightItem, topRightBottomItem])
         
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [leftGroup, rightGroup])
         
